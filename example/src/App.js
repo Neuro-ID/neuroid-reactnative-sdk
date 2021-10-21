4cd9f55b2984be148ca64c43edaff749edc15528
Rajiv <rr@neuro-id.com>
2021-10-21 15:20:29 -0600

    feat(spa): react-native spa initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,41 +1,19 @@
 import * as React from 'react';
-
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RegisterScreen } from './RegisterScreen';
 import { ContactScreen } from './ContactScreen';
-
-// import { NativeModules } from 'react-native';
-// const { configure } = NativeModules;
-
-// const [conf, setConf] = React.useState<String | undefined>();
+import { DefaultForm } from './DefaultForm';
 
 const Stack = createNativeStackNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="Register"
-//           component={HomeScreen}
-//           options={{ title: 'Register' }}
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ title: 'Register' }}
-        />
+      <Stack.Navigator initialRouteName="Neuro ID" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Neuro ID" component={DefaultForm} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Contact" component={ContactScreen} />
       </Stack.Navigator>
     </NavigationContainer>
