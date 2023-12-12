216bbda5c77c137b88dc32d16396d6e540c49373
kurt ishisaka <kurtishisaka@kurts-MacBook-Pro.local>
2023-12-12 09:20:10 -0800

    ENG-6729 update script with proper -i usage

--- scripts/advancedDeviceSetup.sh
+++ scripts/advancedDeviceSetup.sh
@@ -6,29 +6,29 @@ echo "Updating Libraries"
 version=$(sed -n "s/.*'NeuroID', '\([^']*\)'.*/\1/p" neuroid-reactnative-sdk.podspec)
 
 if [ -n "$version" ]; then
-    sed -i '' 's/s.dependency "React-Core"/s.dependency "NeuroID\/AdvancedDevice", '"'$version'"' \n  s.dependency "React-Core"/' neuroid-reactnative-sdk.podspec
+    sed -i='' 's/s.dependency "React-Core"/s.dependency "NeuroID\/AdvancedDevice", '"'$version'"' \n  s.dependency "React-Core"/' neuroid-reactnative-sdk.podspec
 else
-    sed -i '' 's/s.dependency "React-Core"/s.dependency "NeuroID\/AdvancedDevice" \n  s.dependency "React-Core"/' neuroid-reactnative-sdk.podspec
+    sed -i='' 's/s.dependency "React-Core"/s.dependency "NeuroID\/AdvancedDevice" \n  s.dependency "React-Core"/' neuroid-reactnative-sdk.podspec
 fi
 
 # Update Android Libraries 
-sed -i '' 's/neuroid-android-sdk:react-android-sdk/neuroid-android-sdk:react-android-advanced-device-sdk/' android/build.gradle
+sed -i='' 's/neuroid-android-sdk:react-android-sdk/neuroid-android-sdk:react-android-advanced-device-sdk/' android/build.gradle
 
 # Implement code for iOS header and actual
-sed -i '' 's/start:/start:(BOOL)advancedDeviceSignals\n                 withResolver:/' ios/NeuroidReactnativeSdk.m
+sed -i='' 's/start:/start:(BOOL)advancedDeviceSignals\n                 withResolver:/' ios/NeuroidReactnativeSdk.m
 
 # Add to ios function file
-sed -i '' 's/@objc(start:withRejecter:)/@objc(start:withResolver:withRejecter:)/' ios/NeuroidReactnativeSdk.swift
-sed -i '' 's/func start(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {/func start(advancedDeviceSignals: Bool, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {/' ios/NeuroidReactnativeSdk.swift
-sed -i '' 's/NeuroID.start()/NeuroID.start(advancedDeviceSignals: advancedDeviceSignals)/' ios/NeuroidReactnativeSdk.swift
+sed -i='' 's/@objc(start:withRejecter:)/@objc(start:withResolver:withRejecter:)/' ios/NeuroidReactnativeSdk.swift
+sed -i='' 's/func start(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {/func start(advancedDeviceSignals: Bool, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {/' ios/NeuroidReactnativeSdk.swift
+sed -i='' 's/NeuroID.start()/NeuroID.start(advancedDeviceSignals: advancedDeviceSignals)/' ios/NeuroidReactnativeSdk.swift
 
 
 # Implement code for Android actual 
-sed -i '' '/import com.neuroid.tracker.extensions.setVerifyIntegrationHealth/i \
+sed -i='' '/import com.neuroid.tracker.extensions.setVerifyIntegrationHealth/i \
 import com.neuroid.tracker.extensions.start\
 ' android/src/main/java/com/neuroidreactnativesdk/NeuroidReactnativeSdkModule.kt
 
-sed -i '' '/fun start() {/i \
+sed -i='' '/fun start() {/i \
     fun start(advancedDeviceSignals: Boolean) {\
         NeuroID.getInstance()?.setIsRN() \
         NeuroID.getInstance()?.start(advancedDeviceSignals)\
@@ -39,12 +39,12 @@ sed -i '' '/fun start() {/i \
 
 
 # update types
-sed -i '' 's/start: ()/start: (advancedDeviceSignals?: Boolean)/' src/types.ts
+sed -i='' 's/start: ()/start: (advancedDeviceSignals?: Boolean)/' src/types.ts
 
 # update index
-sed -i '' 's/start(): /start(advancedDeviceSignals?: Boolean): /' src/index.tsx
-sed -i '' 's/NeuroidReactnativeSdk.start()/NeuroidReactnativeSdk.start(!!advancedDeviceSignals)/' src/index.tsx
+sed -i='' 's/start(): /start(advancedDeviceSignals?: Boolean): /' src/index.tsx
+sed -i='' 's/NeuroidReactnativeSdk.start()/NeuroidReactnativeSdk.start(!!advancedDeviceSignals)/' src/index.tsx
 
 # update package.json for new module name and description
-sed -i '' 's/"name": "neuroid-reactnative-sdk"/"name": "neuroid-reactnative-sdk-advanced-device"/' package.json
-sed -i '' 's/"description": "Official NeuroID React Native SDK"/"description": "Official NeuroID React Native SDK for Advanced Device Tracking"/' package.json
+sed -i='' 's/"name": "neuroid-reactnative-sdk"/"name": "neuroid-reactnative-sdk-advanced-device"/' package.json
+sed -i='' 's/"description": "Official NeuroID React Native SDK"/"description": "Official NeuroID React Native SDK for Advanced Device Tracking"/' package.json
