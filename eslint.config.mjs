d062e5a82949edc4018e71e9ddc676b8bc0941d5
Collin Dunphy <collin.dunphy@neuro-id.com>
2026-03-13 11:05:01 -0400

    [ENG-11529] Add Formatting and Linting (#290)

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import js from "@eslint/js";
+import sonarjs from "eslint-plugin-sonarjs";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    plugins: {
+      js,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+      },
+    },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [tseslint.configs.recommended],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  sonarjs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "sonarjs/no-ignored-exceptions": "off",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
+  prettier,
+]);
